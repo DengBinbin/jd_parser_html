24f39f0afe702210af8380e81611f948d4fa1f45
akop <779581942@qq.com>
2018-09-16 18:29:46 +0800

    update

--- demo.py
+++ demo.py
@@ -8,6 +8,8 @@ sys.path.append("./src/ipin/parser/jd_parser/")
 from api.api_jd_parser import JdParser
 from utils.path import *
 import codecs
+import json
+
 test = JdParser()
 htmlContent1 = codecs.open(jd_path+'lagou/lagou.html').read()
 htmlContent2 = codecs.open(co_path+'lagou/lagou.html').read()
@@ -15,7 +17,6 @@ htmlContent3 = codecs.open(xz_path+'dajie/dajie.html').read()
 result1 = test.parser(htmlContent=htmlContent1,jdFrom="lagou",type="jd_detail")
 result2 = test.parser(htmlContent=htmlContent2,jdFrom="lagou",type="co")
 result3 = test.parser(htmlContent=htmlContent3,jdFrom="dajie",type="xiaozhao")
-import json
 print u"JD解析"+"*"*30
 print json.dumps(result1,ensure_ascii=False,indent=4)
 print u"CO解析"+"*"*30
